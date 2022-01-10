c998d842a8874f023bb08149fcfa713e631c7eb3
hyun98 <hyun0404woo@gmail.com>
2022-01-10 17:47:50 +0900

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# ActiveSpringBoot
-스프링부트 연습용 레포
--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.jpabook.jpashop;
+package jpabook.jpashop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
