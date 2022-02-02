f9484e4bd6a0af58a49f0af2f9a63a3b8dd4de89
Henu <hyun0404woo@gmail.com>
2022-02-02 20:58:16 +0900

    done

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.service;
 
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,18 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, UpdateItemDto itemDto) {
+        
+        // 가져올때 영속성 엔티티를 가져온다.
+        // jpa의 merge 호출시 수행되는 코드와 같다. 근데 merge는 입력으로 들어오지 않은 필드값은 null로 바꾼다.
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(itemDto.getPrice());
+        findItem.setName(itemDto.getName());
+        findItem.setStockQuantity(itemDto.getStockQuantity());
+        // 트랜잭션 커밋 시점에 변경이 감지되면 flush가 일어나서 업데이트 쿼리를 알아서 날려준다.
     }
 
     public List<Item> findItems() {
